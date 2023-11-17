765308fcc17365c68ec5fd51f017bf1331394eb4
chalscc <carlescatalancalabuig@gmail.com>
2023-11-17 16:45:01 +0100

    Counter y Bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title: string = 'Mi primera app de Angular';
   public counter: number = 10;
+
+  // Por defecto la funcion es publica, no es necesario ponerle el public
+  increaseBy(value:number): void {
+    this.counter += value;
+  }
+  resetCounter(): void {
+    this.counter = 10;
+  }
 }
