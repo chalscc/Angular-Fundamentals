1fce4e853c3851621d8237db698a916806253d76
chalscc <carlescatalancalabuig@gmail.com>
2023-11-17 17:07:55 +0100

    43. Counter functionality

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: `<h1>Hola Counter</h1>`
-})
-export class CounterComponent {
+  template: `
+    <h3> Counter: {{counter}}</h3>
 
+    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="resetCounter()">Reset</button>
+    <button (click)="increaseBy(-1)">-1</button>
+  `
+})
+
+export class CounterComponent {
+  constructor() { }
+
+  public counter: number = 10;
+
+  // Por defecto la funcion es publica, no es necesario ponerle el public
+  increaseBy(value:number): void {
+    this.counter += value;
+  }
+  resetCounter(): void {
+    this.counter = 10;
+  }
 }
