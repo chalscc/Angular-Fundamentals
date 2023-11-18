bab0d4a1c7e54a5f19ba4ba677728ca6260759a6
chalscc <carlescatalancalabuig@gmail.com>
2023-11-18 19:59:51 +0100

    61. Creating new components

---
+++ src/app/dbz/components/character/character.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-character',
+  templateUrl: 'character.component.html'
+})
+
+export class CharacterComponent {
+
+}
