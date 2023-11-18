c23f13075ba8e2dc6da661b5d1105cb1f3565dab
chalscc <carlescatalancalabuig@gmail.com>
2023-11-18 20:53:16 +0100

    ngFor directive options first/last/odd/even

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,9 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent { }
+export class ListComponent {
+
+  @Input() // Permite que el padre le pase información al hijo
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10,
+  }];
+
+}
